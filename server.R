8867caa03d6cb3a8e618cf055c9e442128de0e61
Stephen Pearce <spearce@me.com>
2018-04-16 12:39:43 +1000

    Rearranged matches to be correct and modified formatting

--- server.R
+++ server.R
@@ -82,7 +82,7 @@ shinyServer(function(input, output) {
           class = "split split-one",
           div(
             class = "round round-one current",
-            div(class = "round-details", "Round 1", br(), span(class = "date", "Starting May 23")),
+            div(class = "round-details", "Round 1", br(), span(class = "date", "Starts May 23")),
             matchup(class = "blank"), # RELD1 Bye
             matchup(
               l$Gman$D3[1,],
@@ -92,7 +92,7 @@ shinyServer(function(input, output) {
               l$BigO$D1[4,],
               l$Gman$D6A[1,]
             ),
-            tags$ul(class ="matchup",
+            tags$ul(class = "matchup",
                     team("top", l$REL$D4[1,]),
                     custom_team("bottom", "Best Stunty")
             ),
@@ -108,172 +108,9 @@ shinyServer(function(input, output) {
               l$BigO$D2[1,],
               l$REL$D9E[1,]
             ),
-            matchup(
+            matchup(class = "mid",
               l$BigO$D1[2,],
               l$Gman$D6B[1,]
-            ),
-            matchup(
-              l$REL$D1[2,],
-              l$BigO$D4A[1,]
-            ),
-            matchup(
-              l$REL$D7[1,],
-              l$Gman$D3[2,]
-            ),
-            matchup(
-              l$Gman$D1[5,],
-              l$BigO$D2[3,]
-            ),
-            matchup(
-              l$Gman$D5[1,],
-              l$REL$D7[2,]
-            ),
-            matchup(
-              l$REL$D9D[1,],
-              l$Gman$D1[2,]
-            ),
-            matchup(
-              l$REL$D4[2,],
-              l$Gman$D2[3,]
-            ),
-            matchup(
-              l$BigO$D1[5,],
-              l$REL$D3[3,]
-            ),
-            tags$ul(class ="matchup",
-                    team("top", l$REL$D5[1,]),
-                    custom_team("bottom", "Minors Champion")
-            )
-          ),
-          div(
-            class = "round round-two",
-            div(class = "round-details", "Round 2", br(), span(class = "date", "Starting May 30")),
-            matchup(
-              l$REL$D1[1,],
-              NULL
-            ),
-            matchup(),
-            matchup(),
-            matchup(),
-            matchup(),
-            matchup(),
-            matchup(),
-            matchup()
-          ),
-          div(
-            class = "round round-three",
-            div(class = "round-details", "Round 3", br(), span(class = "date", "Starting June 6")),
-            matchup(),
-            matchup(),
-            matchup(),
-            matchup()
-          ),
-          div(
-            class = "round round-four",
-            div(class = "round-details", "Round 4", br(), span(class = "date", "Starting June 13")),
-            matchup(),
-            matchup()
-          )
-        ),
-        div(
-          class = "champion",
-          div(
-            class = "semis-l",
-            div(
-              class = "round-details",
-              "SF #1",
-              br(),
-              span(class = "date", "Starting June 20")
-            ),
-            matchup(class = "championship")
-          ),
-          div(
-            class = "final",
-            icon("trophy"),
-            div(
-              class = "round-details",
-              "Superbowl",
-              br(),
-              span(class = "date", "Starting June 27")
-            ),
-            matchup(class = "championship")
-          ),
-          div(
-            class = "semis-r",
-            div(
-              class = "round-details",
-              "SF #2",
-              br(),
-              span(class = "date", "Starting June 20")
-            ),
-            matchup(class = "championship")
-          )
-        ),
-        div(
-          class = "split split-two",
-          div(
-            class = "round round-four",
-            div(class = "round-details", "Round 4", br(), span(class = "date", "Starting June 13")),
-            matchup(),
-            matchup()
-          ),
-          div(
-            class = "round round-three",
-            div(class = "round-details", "Round 3", br(), span(class = "date", "Starting June 6")),
-            matchup(),
-            matchup(),
-            matchup(),
-            matchup()
-          ),
-          div(
-            class = "round round-two",
-            div(class = "round-details", "Round 2", br(), span(class = "date", "Starting May 30")),
-            matchup(
-              l$BigO$D1[1,],
-              NULL
-            ),
-            matchup(),
-            matchup(),
-            matchup(),
-            matchup(),
-            matchup(),
-            matchup(),
-            matchup(
-              NULL,
-              l$Gman$D1[1,]
-            )
-          ),
-          div(
-            class = "round round-one current",
-            div(class = "round-details", "Round 1", br(), span(class = "date", "Starting May 23")),
-            matchup(class = "blank"), # BigOD1 Bye
-            matchup(
-              l$REL$D3[1,],
-              l$BigO$D3[2,]
-            ),
-            matchup(
-              l$Gman$D1[4,],
-              l$REL$D9A[1,]
-            ),
-            matchup(
-              l$REL$D2[1,],
-              l$Gman$D6D[1,]
-            ),
-            matchup(
-              l$Gman$D1[3,],
-              l$REL$D9B[1,]
-            ),
-            matchup(
-              l$REL$D8[2,],
-              l$Gman$D2[2,]
-            ),
-            matchup(
-              l$Gman$D4[1,],
-              l$REL$D8[1,]
-            ),
-            matchup(
-              l$REL$D1[4,],
-              l$Gman$D6E[1,]
             ),
             matchup(
               l$REL$D2[3,],
@@ -304,6 +141,170 @@ shinyServer(function(input, output) {
               l$Gman$D5[2,]
             ),
             matchup(class = "blank") # GmanD1 Bye
+          ),
+          div(
+            class = "round round-two",
+            div(class = "round-details", "Round 2", br(), span(class = "date", "Starts May 30")),
+            matchup(
+              l$REL$D1[1,],
+              NULL
+            ),
+            matchup(),
+            matchup(),
+            matchup(class = "mid"),
+            matchup(),
+            matchup(),
+            matchup(),
+            matchup(
+              NULL,
+              l$Gman$D1[1,]
+            )
+          ),
+          div(
+            class = "round round-three",
+            div(class = "round-details", "Round 3", br(), span(class = "date", "Starts June 6")),
+            matchup(),
+            matchup(class = "mid"),
+            matchup(),
+            matchup()
+          ),
+          div(
+            class = "round round-four",
+            div(class = "round-details", "Hype Video Round", br(), span(class = "date", "Starts June 13")),
+            matchup(class = "mid"),
+            matchup()
+          )
+        ),
+        div(
+          class = "champion",
+          div(
+            class = "semis-l",
+            div(
+              class = "round-details",
+              "SF #1",
+              br(),
+              span(class = "date", "Starts June 20")
+            ),
+            matchup(class = "championship")
+          ),
+          div(
+            class = "final",
+            icon("trophy"),
+            div(
+              class = "round-details",
+              "Superbowl",
+              br(),
+              span(class = "date", "Starts June 27")
+            ),
+            matchup(class = "championship")
+          ),
+          div(
+            class = "semis-r",
+            div(
+              class = "round-details",
+              "SF #2",
+              br(),
+              span(class = "date", "Starts June 20")
+            ),
+            matchup(class = "championship")
+          )
+        ),
+        div(
+          class = "split split-two",
+          div(
+            class = "round round-four",
+            div(class = "round-details", "Hype Video Round", br(), span(class = "date", "Starts June 13")),
+            matchup(class = "mid"),
+            matchup()
+          ),
+          div(
+            class = "round round-three",
+            div(class = "round-details", "Round 3", br(), span(class = "date", "Starts June 6")),
+            matchup(),
+            matchup(class = "mid"),
+            matchup(),
+            matchup()
+          ),
+          div(
+            class = "round round-two",
+            div(class = "round-details", "Round 2", br(), span(class = "date", "Starts May 30")),
+            matchup(),
+            matchup(),
+            matchup(),
+            matchup(class = "mid"),
+            matchup(
+              l$BigO$D1[1,],
+              NULL
+            ),
+            matchup(),
+            matchup(),
+            matchup()
+          ),
+          div(
+            class = "round round-one current",
+            div(class = "round-details", "Round 1", br(), span(class = "date", "Starts May 23")),
+            matchup(
+              l$REL$D1[2,],
+              l$BigO$D4A[1,]
+            ),
+            matchup(
+              l$REL$D7[1,],
+              l$Gman$D3[2,]
+            ),
+            matchup(
+              l$Gman$D1[5,],
+              l$BigO$D2[3,]
+            ),
+            matchup(
+              l$Gman$D5[1,],
+              l$REL$D7[2,]
+            ),
+            matchup(
+              l$REL$D9D[1,],
+              l$Gman$D1[2,]
+            ),
+            matchup(
+              l$REL$D4[2,],
+              l$Gman$D2[3,]
+            ),
+            matchup(
+              l$BigO$D1[5,],
+              l$REL$D3[3,]
+            ),
+            tags$ul(class ="matchup mid",
+                    team("top", l$REL$D5[1,]),
+                    custom_team("bottom", "Minors Champion")
+            ),
+            matchup(class = "blank"), # BigOD1 Bye
+            matchup(
+              l$REL$D3[1,],
+              l$BigO$D3[2,]
+            ),
+            matchup(
+              l$Gman$D1[4,],
+              l$REL$D9A[1,]
+            ),
+            matchup(
+              l$REL$D2[1,],
+              l$Gman$D6D[1,]
+            ),
+            matchup(
+              l$Gman$D1[3,],
+              l$REL$D9B[1,]
+            ),
+            matchup(
+              l$REL$D8[2,],
+              l$Gman$D2[2,]
+            ),
+            matchup(
+              l$Gman$D4[1,],
+              l$REL$D8[1,]
+            ),
+            matchup(
+              l$REL$D1[4,],
+              l$Gman$D6E[1,]
+            )
+            
           )
         )
       )
